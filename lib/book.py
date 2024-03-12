4cd8578f99e62d13ae595a679f7b7ec7bb1a0544
Emilymatoke <emilymomata@gmail.com>
2024-03-12 09:33:34 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
+from book import Book
 
-class Book:
-    pass
+class Book :
+    def __init__(self, title = "And Then There Were None", page_count = 272) :
+        self.title = title
+        self.page_count = page_count
+    
+    
+    @property
+    def title(self) :
+        "The title property"
+        return self._title
+    
+    @title.setter
+    def title(self, title) :
+        "And Then There Were None"
+        self._title = title
+
+
+    @property
+    def page_count(self) :
+        "The page_count property"
+        return self. _page_count
+    
+    @page_count.setter
+    def page_count (self, page_count) :
+        "Should be an integer"
+        if page_count is int :
+            self._page_count = page_count
+        else:
+            return "It is not an integer" 
+
+
+    
+        
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand = "Adidas", size = 9) :
+        self.brand = brand
+        self.size = size
+
+    @property
+    def brand (self) :
+        "The brand property "
+        return self._brand
+    
+    @brand.setter
+    def brand(self,brand) :
+        "Adidas"
+        self._brand = brand
+
+
+    @property
+    def size(self) :
+        "The size property"
+        return self._size
+
+    @size.setter
+    def size(self, size) :
+        "Size must be an integer"
+        if size is int :
+            self._size = size
+        else :
+            return "not an integer"
+        
